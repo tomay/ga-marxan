b3d0501d55716cfd9eafe790424dd3537249cb6f
tomay <tom.allnutt@gmail.com>
2013-01-26 19:02:21 -0800

    species

--- ruby-obj-function/species.rb
+++ ruby-obj-function/species.rb
@@ -1,12 +1,11 @@
 require 'csv'
-# open puvsp file
+# open files
 sppf = "inputs/species.csv"
-
-# open spp file
-puvf = "puvsp_spp_test.csv"
-
+puvf = "inputs/puvsp_spp_test.csv"
 puvfile = File.open(puvf,"r")
 sppfile = File.open(sppf,"r")
+
+selection = [1,2,3,4]
 
 puv = {}
 spp = {}
@@ -35,7 +34,7 @@ puvfile.each {|row|
   end
   
   h = {}
-   if puv[pu_id] == nil
+  if puv[pu_id] == nil
     h = {"species" => [spec_id], "amount" => [amount]}
   else
     h = puv[pu_id] # capture existing values
@@ -47,3 +46,41 @@ puvfile.each {|row|
 # catch final set
 spp_sums[spp_init] = {:total_area => sum}
 puvfile.close
+
+# count spp amounts on a selected set of pus
+selected = {}
+penalty = 0
+selection.each {|id|
+  (0..puv[id]["species"].count - 1).each {|i| 
+    species = puv[id]["species"][i]
+    area = puv[id]["amount"][i]
+    h = {}
+    if selected[species] == nil
+      h = {"selected_amount" => area}
+    else
+      area = area + selected[species]["selected_amount"] # capture existing values
+      h["selected_amount"] = area
+    end
+    selected[species] = h
+  }
+}
+#puts "Perimeter: " + perimeter.to_s
+
+# compare selected amounts to targets 
+# requires three files spp, spp_sums and selected
+penalty = 0
+spp.each {|species|
+  #puts species
+  sp_id = species[0]
+  prop = species[1][:prop]
+  spf = species[1][:spf]
+  if spp_sums[sp_id].nil? # no occurrences
+  	penalty = penalty + spf
+  else
+  	total = spp_sums[sp_id][:total_area]
+    area = selected[sp_id]["selected_amount"]
+    if area < prop * total
+      penalty = penalty + spf 
+    end
+  end
+}
